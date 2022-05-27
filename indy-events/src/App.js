18c60e6e6c9f475857f2a16b203b1b920c0ae531
Allison Reynolds <allisonreynolds@Allisons-Air.attlocal.net>
2022-05-27 13:02:29 -0500

    map function

--- indy-events/src/App.js
+++ indy-events/src/App.js
@@ -3,6 +3,18 @@ import axios from "axios";
 import Events from "../../indy-events/src/components/events";
 import { useEffect, useState } from "react";
 import Pagination from "../../indy-events/src/components/pagination";
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+
+const API_KEY = "AIzaSyBuTRGEarfo23MfTdcAEPWj2LYqWrgQYCY";
+function Map() {
+  return (
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 39.7684, lng: 86.1581 }}
+    />
+  );
+}
+const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 const API_URL = "http://localhost:3000/api/v1/events";
 
@@ -14,7 +26,7 @@ async function getAPIdata() {
 function App() {
   const [events, setEvents] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [eventsPerPage, setEventsPerPage] = useState(5);
+  const [eventsPerPage] = useState(5);
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -33,6 +45,14 @@ function App() {
   return (
     <div className="App">
       <Events events={currentEvents} />
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <WrappedMap
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${API_KEY}`}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </div>
       <Pagination
         eventsPerPage={eventsPerPage}
         totalEvents={events.length}
