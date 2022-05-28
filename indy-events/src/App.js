4e133f73022abe57b802e5a489e410dd89946757
ALLISON <allisonreynoldsc@gmail.com>
2022-05-28 09:57:22 -0500

    removed first react app because of errors I could not resolve so I started over

--- indy-events/src/App.js
+++ indy-events/src/App.js
@@ -3,18 +3,6 @@ import axios from "axios";
 import Events from "../../indy-events/src/components/events";
 import { useEffect, useState } from "react";
 import Pagination from "../../indy-events/src/components/pagination";
-import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
-
-const API_KEY = "AIzaSyBuTRGEarfo23MfTdcAEPWj2LYqWrgQYCY";
-function Map() {
-  return (
-    <GoogleMap
-      defaultZoom={10}
-      defaultCenter={{ lat: 39.7684, lng: 86.1581 }}
-    />
-  );
-}
-const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 const API_URL = "http://localhost:3000/api/v1/events";
 
@@ -45,14 +33,6 @@ function App() {
   return (
     <div className="App">
       <Events events={currentEvents} />
-      <div style={{ width: "100vw", height: "100vh" }}>
-        <WrappedMap
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${API_KEY}`}
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `400px` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        />
-      </div>
       <Pagination
         eventsPerPage={eventsPerPage}
         totalEvents={events.length}
